b5e4ffc758e52e72d53e070c886829a3fc149579
Aksh1602 <155544457+Aksh1602@users.noreply.github.com>
2025-05-12 16:27:09 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,12 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest'
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/fileTransformer.js'
   },
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
-  }
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/admin/src/$1'
+  },
+  modulePathIgnorePatterns: ['<rootDir>/frontend']
 };
